b0bee118d5500503a9aa30a508eccd88b5c55db5
Greg Frasco <frascog17@gmail.com>
2020-07-14 19:32:17 -0400

    feat: add server

---
+++ server/app.py
@@ -0,0 +1,8 @@
+from flask import Flask
+app = Flask(__name__)
+app = Flask(__name__, static_folder='../client/build', static_url_path='/')
+
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
